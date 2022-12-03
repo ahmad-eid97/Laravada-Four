600c11b41ef8e2fdb8cbbfef16144b6ca780c089
civil.ahmad.eid <civil.ahmad.eid@gmail.com>
2022-12-03 13:15:57 +0200

    add home banner data

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,52 @@
 <template>
   <div class="home">
-    <app-home-intro></app-home-intro>
+    <app-home-intro :bannerHead="bannerHead"></app-home-intro>
     <app-home-partners :partners="partners"></app-home-partners>
     <app-home-features :features="features"></app-home-features>
     <app-home-solutions :solutions="solutions"></app-home-solutions>
     <app-home-banner :latestBlog="latestBlog"></app-home-banner>
     <app-home-sections :services="services"></app-home-sections>
-    <app-home-bottom-banner></app-home-bottom-banner>
+    <app-home-bottom-banner
+      :bottomBanner="bottomBanner"
+    ></app-home-bottom-banner>
   </div>
 </template>
 
 <script>
-import AppHomeBanner from '../components/home/AppHomeBanner.vue'
-import AppHomeIntro from '../components/home/AppHomeIntro.vue'
-import AppHomePartners from '../components/home/AppHomePartners.vue'
-import AppHomeFeatures from '../components/home/AppHomeFeatures.vue'
-import AppHomeSolutions from '../components/home/AppHomeSolutions.vue'
-import AppHomeSections from '../components/home/AppHomeSections.vue'
-import AppHomeBottomBanner from '../components/home/AppHomeBottomBanner.vue'
+import AppHomeBanner from "../components/home/AppHomeBanner.vue";
+import AppHomeIntro from "../components/home/AppHomeIntro.vue";
+import AppHomePartners from "../components/home/AppHomePartners.vue";
+import AppHomeFeatures from "../components/home/AppHomeFeatures.vue";
+import AppHomeSolutions from "../components/home/AppHomeSolutions.vue";
+import AppHomeSections from "../components/home/AppHomeSections.vue";
+import AppHomeBottomBanner from "../components/home/AppHomeBottomBanner.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   async asyncData({ $axios }) {
-    const partners = await $axios.get('/partners');
+    const bannerHead = await $axios.get("/sections/banner");
 
-    const features = await $axios.get('/sections/features');
+    const partners = await $axios.get("/partners");
 
-    const solutions = await $axios.get('/sections/solutions');
+    const features = await $axios.get("/sections/features");
 
-    const latestBlog = await $axios.get('/blogs?latest=1');
+    const solutions = await $axios.get("/sections/solutions");
 
-    const services = await $axios.get('/services');
+    const latestBlog = await $axios.get("/blogs?latest=1");
+
+    const services = await $axios.get("/services");
+
+    const bottomBanner = await $axios.get("/sections/banner-bottom");
 
     return {
+      bannerHead: bannerHead.data.data,
       partners: partners.data.data.partners,
       features: features.data.data,
       solutions: solutions.data.data,
       latestBlog: latestBlog.data.data.blogs.slice(0, 1),
       services: services.data.data.services,
-    }
+      bottomBanner: bottomBanner.data.data,
+    };
   },
   components: {
     AppHomeIntro,
@@ -47,10 +55,8 @@ export default {
     AppHomeFeatures,
     AppHomeSolutions,
     AppHomeSections,
-    AppHomeBottomBanner
+    AppHomeBottomBanner,
   },
-}
+};
 </script>
-<style>
-  
-</style>
+<style></style>
