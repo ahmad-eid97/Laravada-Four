87ceedf7bcd4005d5a6f8279d3ec2d172a124f3e
civil.ahmad.eid <civil.ahmad.eid@gmail.com>
2022-12-21 21:38:34 +0200

    add courses pages

---
+++ components/courseDetails/courseIntro.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="courseIntro">
+    <div class="row align-items-center">
+      <div class="col-lg-6">
+        <img :src="courseDetails.image" alt="courseImage" />
+      </div>
+      <div class="col-lg-6">
+        <div class="details">
+          <div class="rate">
+            <b-form-rating
+              color="#f39c12"
+              :inline="true"
+              :no-border="true"
+              value="5"
+              readonly
+            ></b-form-rating>
+            <span>( 1 Customer Reviews )</span>
+          </div>
+
+          <h1>{{ courseDetails.title }}</h1>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+            vel metus malesuada ante imperdiet lobortis.
+          </p>
+
+          <div class="price">
+            <p class="pre">${{ courseDetails.previous_price }}</p>
+            <p>${{ courseDetails.current_price }}</p>
+          </div>
+
+          <button>Add to cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["courseDetails"],
+};
+</script>
+
+<style scoped lang="scss">
+output {
+  padding: 0;
+}
+.courseIntro {
+  margin-top: 100px;
+  img {
+    @include md {
+      margin-bottom: 30px;
+    }
+  }
+  .details {
+    h1 {
+      margin-top: 20px;
+    }
+    p {
+      font-size: 1.5rem;
+      opacity: 0.8;
+    }
+    .price {
+      display: flex;
+      align-items: center;
+      gap: 10px;
+      margin-bottom: 20px;
+      p {
+        font-size: 1.5rem;
+        opacity: 0.7;
+        margin: 0;
+        &:not(.pre) {
+          color: var(--main-color);
+          font-weight: bold;
+        }
+        &.pre {
+          text-decoration: line-through;
+        }
+      }
+    }
+    button {
+      padding: 10px 40px;
+      font-size: 1.3rem;
+      border: 1px solid var(--main-color);
+      color: #fff;
+      background-color: var(--main-color);
+      &:hover {
+        background-color: transparent;
+        color: var(--main-color);
+      }
+    }
+  }
+}
+</style>
---
+++ components/courses/coursesList.vue
@@ -0,0 +1,164 @@
+<template>
+  <div class="coursesList">
+    <h1>Courses We Provide</h1>
+    <div class="row justify-content-center">
+      <div
+        class="col-md-6 col-xl-4"
+        v-for="course in coursesList"
+        :key="course"
+      >
+        <div class="course">
+          <div class="header">
+            <span class="sale">SALE</span>
+            <img
+              :src="course.image"
+              alt="courseImage"
+              @click="$router.push(localePath(`/course/${course.id}`))"
+            />
+            <div class="cart" :class="$i18n.locale === 'ar' ? 'arabic' : ''">
+              <span class="text">Add To Cart</span>
+              <span><i class="fa-regular fa-cart-plus"></i></span>
+            </div>
+          </div>
+          <h2 @click="$router.push(localePath(`/course/${course.id}`))">
+            {{ course.title }}
+          </h2>
+          <div class="price">
+            <p class="pre">${{ course.previous_price }}</p>
+            <p>${{ course.current_price }}</p>
+          </div>
+          <b-form-rating
+            color="#f39c12"
+            :inline="true"
+            :no-border="true"
+            value="5"
+            readonly
+          ></b-form-rating>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-lg-12 col-md-12 text-center">
+      <div class="pagination-area">
+        <a href="#" class="page-numbers">
+          <i class="fa-solid fa-arrow-left"></i>
+        </a>
+        <span class="page-numbers current" aria-current="page">1</span>
+        <a href="#" class="page-numbers">2</a>
+        <a href="#" class="page-numbers">3</a>
+        <a href="#" class="page-numbers">
+          <i class="fa-solid fa-arrow-right"></i>
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["coursesList"],
+};
+</script>
+
+<style scoped lang="scss">
+output {
+  padding: 0;
+}
+.coursesList {
+  margin-top: 100px;
+  h1 {
+    margin-bottom: 50px;
+    text-align: center;
+  }
+  .course {
+    margin-bottom: 30px;
+    .header {
+      position: relative;
+      .sale {
+        position: absolute;
+        top: 10px;
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        background-color: var(--main-color);
+        color: #fff;
+        font-size: 1.3rem;
+        display: grid;
+        place-items: center;
+        z-index: 2;
+        margin: 0 10px;
+      }
+      .cart {
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        font-size: 1.3rem;
+        display: flex;
+        align-items: center;
+        animation: all 0.3s ease-in-out;
+        cursor: pointer;
+        .text {
+          background-color: #fff;
+          padding: 5px 20px;
+          transform: translateX(100px);
+          opacity: 0;
+          position: relative;
+          z-index: 1;
+        }
+        i {
+          color: var(--main-color);
+          background-color: #fff;
+          padding: 5px 20px;
+          position: relative;
+          z-index: 2;
+        }
+        &.arabic {
+          right: unset;
+          left: 0;
+          .text {
+            transform: translateX(-100px);
+          }
+        }
+        &:hover {
+          .text {
+            transform: translateX(0px);
+            opacity: 1;
+          }
+        }
+      }
+      img {
+        width: 100%;
+        height: 300px;
+        cursor: pointer;
+        &:hover {
+          filter: brightness(0.8);
+        }
+      }
+    }
+    h2 {
+      margin-top: 20px;
+      cursor: pointer;
+    }
+    .price {
+      display: flex;
+      align-items: center;
+      gap: 10px;
+      p {
+        font-size: 1.3rem;
+        opacity: 0.7;
+        margin: 0;
+        &:not(.pre) {
+          color: var(--main-color);
+          font-weight: bold;
+        }
+        &.pre {
+          text-decoration: line-through;
+        }
+      }
+    }
+  }
+  .pagination-area {
+    margin: 50px 0;
+  }
+}
+</style>
